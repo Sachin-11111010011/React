0e2f17cbe138400acb1debfd029e05e2cb90394c
Sachin Adhikari <leetcode0008@gmail.com>
2024-10-09 12:03:49 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,6 @@
+const heading=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"this is h1 tag"),React.createElement("h2",{},"this is h2 tag")]
+    ))
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
---
+++ app2.js
@@ -0,0 +1,6 @@
+const heading=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"Im a h1 tag"),React.createElement("h2",{},"Im a h2 tag")])
+);
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
