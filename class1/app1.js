5e0b281934cd5ba5c1a2c193c6ce0cc67faa3efb
Sachin-11111010011 <sachinadhikari0008@gmail.com>
2024-10-13 14:04:49 +0530

    FunctionalComponent

--- class1/app1.js
+++
@@ -1,9 +0,0 @@
-const grandparent=React.createElement("div",
-{id:"grandparent"},
-    React.createElement("div",{id:"parent"},
-        React.createElement("div",{id:"child"},
-           [ React.createElement("h1",{},"Hello im a child"),
-            React.createElement("h2",{},"Hello im a child2")]
-        )));
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(grandparent);
---
+++ src/main.jsx
@@ -0,0 +1,55 @@
+import React  from "react";
+import ReactDOM from "react-dom/client"
+
+const elem=<span >Hello JavaScript</span>;
+
+const HeadingComponent= () =>
+(
+    <div id="container">
+        <h1 className="heading">Namaste React Functional Component</h1>
+    </div>
+);
+
+
+const title=(
+    <h1 className="head" tabIndex="5">
+        {elem} <br />
+        Namaste React using JSX
+        <HeadingComponent></HeadingComponent>
+    </h1>
+)
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(title);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const heading= React.createElement("h1",{id: "heading"},"Hello React")
+// const root=ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading);
+
+// How to create component by using JSX
+
+// const jsxHeading=<h1 id="Head">Hello Namaste React</h1>
+// const root1=ReactDOM.createRoot(document.getElementById("root"))
+// root1.render(jsxHeading);
+
+// const Heading= () =>
+// {
+//     return    <h1> Namaste React</h1>    
+// };  
+
+
+// const jsxHeading1=<h1 id="hello">hello player</h1>
+// const root=ReactDOM.createRoot(document.getElementById("root"))
+// root.render(jsxHeading1)
