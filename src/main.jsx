0bf3091b672dc4a460d9e66eeb837f4d38ee65ef
Sachin-11111010011 <sachinadhikari0008@gmail.com>
2024-10-17 14:01:08 +0530

    Food Delivery

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,88 @@
-import React  from "react";
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const elem=<span >Hello JavaScript</span>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://png.pngtree.com/png-clipart/20230520/original/pngtree-burger-logo-png-image_9166715.png"
+          alt="Logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaeBlIywcv4RMaVlvzAk-ygkpjyiX0juwM6Q&s" alt="" />  
+      <h3>Meghna Foods</h3>
+      <h4>Biryani,North Indian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search"> </div>
+      <div className="res-containers">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );n
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
+
+
+
+// const elem=<span >Hello JavaScript</span>;
 
 // Functional Component
 
-const HeadingComponent= () =>
-(
-    <div id="container">
-        <h1 className="heading">Namaste React Functional Component</h1>
-    </div>
-);
+// const HeadingComponent= () =>
+// (
+//     <div id="container">
+//         <h1 className="heading">Namaste React Functional Component</h1>
+//     </div>
+// );
 
 
-const title=(
-    <h1 className="head" tabIndex="5">
-        {elem} <br />
-        Namaste React using JSX
-        <HeadingComponent></HeadingComponent>
-    </h1>
-)
-
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(title);
-
-
-
-
-
-
-
-
+// const title=(
+//     <h1 className="head" tabIndex="5">
+//         {elem} <br />
+//         Namaste React using JSX
+//         <HeadingComponent></HeadingComponent>
+//     </h1>
+// )
 
 
 
@@ -53,3 +105,6 @@ root.render(title);
 // const jsxHeading1=<h1 id="hello">hello player</h1>
 // const root=ReactDOM.createRoot(document.getElementById("root"))
 // root.render(jsxHeading1)
+
+
+  
