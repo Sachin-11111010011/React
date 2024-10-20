f436cd2a522e701b45abe07b32372dd655d1e9b0
Sachin-11111010011 <sachinadhikari0008@gmail.com>
2024-10-20 14:53:57 +0530

    swiggy

--- src/main.jsx
+++ src/main.jsx
@@ -23,28 +23,138 @@ const Header = () => {
     </div>
   );
 };
-
-const RestaurantCard = () => {
+const styleCard =           
+{
+  backgroundColor: "#f0f0f0"
+}
+const RestaurantCard = (props) => {
+ const {resData}= props;
   return (
-    <div className="res-card">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaeBlIywcv4RMaVlvzAk-ygkpjyiX0juwM6Q&s" alt="" />  
-      <h3>Meghna Foods</h3>
-      <h4>Biryani,North Indian</h4>
-      <h4>4.4 stars</h4>
-      <h4>38 minutes</h4>
-
+    <div className="res-card" style={styleCard}>   
+        <img className="img " height="140px" width="210px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaeBlIywcv4RMaVlvzAk-ygkpjyiX0juwM6Q&s" alt="" />  
+      <h3>{resData.info.resName}</h3>
+      <h4>{resData.info.cuisines.join(" , ")}</h4> 
+      <h4>{resData.info.avgRating}</h4> 
+      <h4>{resData.info.costForTwo / 100} FOR TWO</h4>
+      <h4>{resData.info.deliveryTime}minutes</h4>
     </div>
   );
 };
-
+const resObj =
+{
+  "info": {
+                      "id": "240669",
+                      "name": "Burger King",
+                      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/8/9/ba3ea4d4-e498-4b7e-aa29-ef3b5f240f43_240669.jpg",
+                      "locality": "Indira Nagar",
+                      "areaName": "Adarsh Nagar",
+                      "costForTwo": "₹350 for two",
+                      "cuisines": [
+                        "Burgers",
+                        "American"
+                      ],
+                      "avgRating": 4.3,
+                      "parentId": "166",
+                      "avgRatingString": "4.3",
+                      "totalRatingsString": "17K+",
+                      "sla": {
+                        "deliveryTime": 42,
+                        "lastMileTravel": 7.2,
+                        "serviceability": "SERVICEABLE",
+                        "slaString": "40-45 mins",
+                        "lastMileTravelString": "7.2 km",
+                        "iconType": "ICON_TYPE_EMPTY"
+                      },
+                      "availability": {
+                        "nextCloseTime": "2024-10-21 04:00:00",
+                        "opened": true
+                      },
+                      "badges": {
+                        "imageBadges": [
+                          {
+                            "imageId": "Green%20Dot%20Awards/Best%20In%20Veg%20Burger.png",
+                            "description": "Delivery!"
+                          },
+                          {
+                            "imageId": "Rxawards/_CATEGORY-Burger.png",
+                            "description": "Delivery!"
+                          }
+                        ]
+                      },
+                      "isOpen": true,
+                      "type": "F",
+                      "badgesV2": {
+                        "entityBadges": {
+                          "textBased": {
+                            
+                          },
+                          "imageBased": {
+                            "badgeObject": [
+                              {
+                                "attributes": {
+                                  "imageId": "Green%20Dot%20Awards/Best%20In%20Veg%20Burger.png",
+                                  "description": "Delivery!"
+                                }
+                              },
+                              {
+                                "attributes": {
+                                  "imageId": "Rxawards/_CATEGORY-Burger.png",
+                                  "description": "Delivery!"
+                                }
+                              }
+                            ]
+                          },
+                          "textExtendedBadges": {
+                            
+                          }
+                        }
+                      },
+                      "aggregatedDiscountInfoV3": {
+                        "header": "60% OFF",
+                        "subHeader": "UPTO ₹120"
+                      },
+                      "differentiatedUi": {
+                        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                        "differentiatedUiMediaDetails": {
+                          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                          "lottie": {
+                            
+                          },
+                          "video": {
+                            
+                          }
+                        }
+                      },
+                      "reviewsSummary": {
+                        
+                      },
+                      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+                      "restaurantOfferPresentationInfo": {
+                        
+                      },
+                      "externalRatings": {
+                        "aggregatedRating": {
+                          "rating": "--"
+                        }
+                      },
+                      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+                    },
+                    "analytics": {
+                      
+                    },
+                    "cta": {
+                      "link": "swiggy://menu?restaurant_id=240669",
+                      "text": "RESTAURANT_MENU",
+                      "type": "DEEPLINK"
+                    }
+                  }
+                  
 const Body = () => {
   return (
     <div className="body">
       <div className="search"> </div>
       <div className="res-containers">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        <RestaurantCard resData={resObj}/>
       </div>
     </div>
   );n
